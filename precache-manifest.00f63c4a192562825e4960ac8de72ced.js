7e7bd0428dee407772cec14fe06280664e059ade
Hemmy Bastawi <dev.ibrahembastawi@gmail.com>
2019-04-28 04:18:47 +0200

    Updates

---
+++ precache-manifest.00f63c4a192562825e4960ac8de72ced.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf36f093b39998fc72cb",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/css/main.3c1594a4.chunk.css"
+  },
+  {
+    "revision": "bf36f093b39998fc72cb",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/js/main.bf36f093.chunk.js"
+  },
+  {
+    "revision": "b848fdff04c876437404",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/js/1.b848fdff.chunk.js"
+  },
+  {
+    "revision": "84817fe609b28e4c02e4",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/js/runtime~main.84817fe6.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/frontend-nanodegree-MyReads-remaster/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "3f9825b0e7ae5c3c4f016120d3a32e69",
+    "url": "/frontend-nanodegree-MyReads-remaster/index.html"
+  }
+];
